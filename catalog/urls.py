682ab9d4840fcdfe53d7b38b025fe14683d33ed7
PinderKingui1488 <ananimuszahar@gmail.com>
2025-02-07 14:01:01 +0300

    GOIDAAAA!

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,23 +1,14 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from catalog.views import (
-    ProductListView,
-    ProductDetailView,
-    ProductCreateView,
-    ProductUpdateView,
-    ProductDeleteView,
-)
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="products_list"),
-    path("catalog/<int:pk>/", ProductDetailView.as_view(), name="products_detail"),
-    path("catalog/create", ProductCreateView.as_view(), name="products_create"),
-    path(
-        "catalog/<int:pk>/update", ProductUpdateView.as_view(), name="products_update"
-    ),
-    path(
-        "catalog/<int:pk>/delete", ProductDeleteView.as_view(), name="products_delete"
-    ),
+    path("catalogs/<int:pk>/", (ProductDetailView.as_view()), name="products_detail"),
+    path("catalogs/create", ProductCreateView.as_view(), name="products_create"),
+    path("catalogs/<int:pk>/update", ProductUpdateView.as_view(), name="products_update"),
+    path("catalogs/<int:pk>/delete", ProductDeleteView.as_view(), name="products_delete"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView,
-)
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm, ProductModeratorForm
 from catalog.models import Product
-from catalog.forms import ProductForm
-
-
-def home(request):
-    return render(request, "home.html")
-
-
-def contacts(request):
-    return render(request, "contacts.html")
 
 
 class ProductListView(ListView):
@@ -27,17 +14,38 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_path'] = self.request.path
+        return context
 
-class ProductCreateView(CreateView):
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:products_list")
 
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
 
-class ProductUpdateView(UpdateView):
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:products_list")
+
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner or user.has_perm("catalog.update_product"):
+            return ProductForm
+        if user.has_perm("catalog.can_unpublish_product") and user.has_perm(
+                "catalog.delete_product"
+        ):
+            return ProductModeratorForm
+        raise PermissionDenied
 
 
 class ProductDeleteView(DeleteView):
